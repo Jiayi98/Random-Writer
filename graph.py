62139e20a702f1987a627b9be83bedf7970ff6ba
jiayi98 <judyzhou959@utexas.edu>
2018-10-29 10:55:26 -0500

    just create the file

---
+++ graph.py
@@ -0,0 +1,17 @@
+class MorkovChainGraph(object):
+    """A constuctor that can read the input
+    and build according probability model
+    """
+    def __init__(self, level.......):
+
+    def read(self,......):
+
+    """ A node class: state is a node; directed edge with weight(probability) from state to next token; next is the character with the most probability following the state; newState is the sate that derives from the original state and the new character
+    """
+    class Node:
+        def __init__(self,.........):
+            self.state
+            self.edge
+            self.next
+            self.newState
+
